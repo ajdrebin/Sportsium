102fef4ac5f79f0a2d5f77142ffe77b63610687a
adrebin <alina@drebin.com>
2019-10-20 17:23:11 -0400

    ML-test

---
+++ ML-test/ML-test/OpenCVWrapper.h
@@ -0,0 +1,14 @@
+//
+//  OpenCVWrapper.h
+//  ML-test
+//
+//  Created by Alina Drebin on 10/20/19.
+//  Copyright © 2019 Alina Drebin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenCVWrapper : UIViewController
++ (NSString *)openCVVersionString;
+- (void)isThisWorking;
+@end
