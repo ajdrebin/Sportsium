49a9449e4aacf80f0654b9fe36ea8104a1c24018
jesscheng <jesca@umich.edu>
2019-10-22 21:06:59 -0400

    testing contributor issue

--- Sportsium/Sportsium/CheckInViewController.swift
+++ Sportsium/Sportsium/CheckInViewController.swift
@@ -25,7 +25,6 @@ class CheckInViewController: UIViewController {
     
     var dataSource = [String]()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
