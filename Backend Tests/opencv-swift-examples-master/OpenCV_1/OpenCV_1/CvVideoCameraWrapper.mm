1e61d0cbce91cb369ed076dc52d845c3c6dc675c
adrebin <alina@drebin.com>
2019-10-29 14:45:46 -0400

    working opencv skeleton

--- Backend Tests/opencv-swift-examples-master/OpenCV_1/OpenCV_1/CvVideoCameraWrapper.mm
+++ Backend Tests/opencv-swift-examples-master/OpenCV_1/OpenCV_1/CvVideoCameraWrapper.mm
@@ -30,7 +30,7 @@ using namespace std;
 {
     videoCamera = [[CvVideoCamera alloc] initWithParentView:iv];
     
-    videoCamera.defaultAVCaptureDevicePosition = AVCaptureDevicePositionFront;
+    videoCamera.defaultAVCaptureDevicePosition = AVCaptureDevicePositionBack;
     videoCamera.defaultAVCaptureSessionPreset = AVCaptureSessionPreset352x288;
     videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationPortrait;
     videoCamera.defaultFPS = 30;
@@ -66,41 +66,107 @@ using namespace std;
      cv::Scalar upper_green(70,255,255);
 
     //blue range
-    cv::Scalar lower_blue(110,50,50);
+//    cv::Scalar lower_blue(110,50,50);
+//    cv::Scalar upper_blue(130,255,255);
+//    cv::Scalar lower_blue(0,0,150);
+//    cv::Scalar upper_blue(70,70,255);
+    cv::Scalar lower_blue(100,100,20);
     cv::Scalar upper_blue(130,255,255);
 
     //red range
     cv::Scalar lower_red(0,31,255);
     cv::Scalar upper_red(176,255,255);
+//    cv::Scalar lower_red(0,0,0);
+//    cv::Scalar upper_red(15,80,80);
 
     //white range
     cv::Scalar lower_white(0,0,0);
     cv::Scalar upper_white(0,0,255);
 
     cv::inRange(HSV, lower_green, upper_green, mask);
+//    cout << "mask: " << mask << endl;
+    cout << "size: " << mask.size() << endl;
+    cout << "camera size: " << cameraFeed.size() << endl;
     cv::Mat res, res_bgr, res_gray;
-    cv::bitwise_and(cameraFeed, cameraFeed, res, mask=mask);
+    cv::bitwise_and(cameraFeed, cameraFeed, res, mask);
+//    cv::bitwise_and(<#InputArray src1#>, <#InputArray src2#>, <#OutputArray dst#>)
     
 //    cout << "res: " << res << endl;
-    cv::cvtColor(res, res_bgr, CV_HSV2BGR);
+//    cv::cvtColor(res, res_bgr, CV_HSV2BGR);
 //    cv::cvtColor(res, res_gray, CV_BGR2HSV);
-    cv::cvtColor(res_bgr, res_gray, CV_BGR2GRAY);
-    return cameraFeed;
+    cv::cvtColor(res, res_gray, CV_BGR2GRAY);
+//    return res;
 //
-//    cv::Mat kernel = getStructuringElement( cv::MORPH_RECT, cvSize(13,13));
-//    cv::Mat thresh;
-//    cv::threshold(res_gray, thresh, 127, 255, cv::THRESH_BINARY_INV | cv::THRESH_OTSU);
-//    cv::morphologyEx(thresh, thresh, cv::MORPH_TOPHAT, kernel);
+    cv::Mat kernel = getStructuringElement( cv::MORPH_RECT, cvSize(13,13));
+    cv::Mat thresh;
+    cv::threshold(res_gray, thresh, 127, 255, cv::THRESH_BINARY_INV | cv::THRESH_OTSU);
+    cv::morphologyEx(thresh, thresh, cv::MORPH_CLOSE, kernel);
 //
-//    cv::Mat temp;
-//    thresh.copyTo(temp);
-//    vector< vector<cv::Point> > contours;
-//    vector<cv::Vec4i> hierarchy;
-//
-//    findContours( temp, contours, hierarchy, CV_RETR_EXTERNAL,  CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+    cv::Mat temp;
+    thresh.copyTo(temp);
+    vector< vector<cv::Point> > contours;
+    vector<cv::Vec4i> hierarchy;
+
+    findContours( temp, contours, hierarchy, CV_RETR_TREE,  CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
 //    cout << "hit: " << res << endl;
 //    cout << "contours: " << contours.size() << endl;
 //    cout << "hierarchy: " << hierarchy.size() << endl;
+    
+
+    int font = cv::FONT_HERSHEY_SIMPLEX;
+//     contours.resize(contours0.size());
+    cout << contours.size() << endl;
+    for( size_t k = 0; k < contours.size(); k = k + 1 ) {
+        cv::Rect rect = cv::boundingRect(contours[k]);
+//        cout << rect << endl;
+//        cout << rect.x << endl;
+//        cout << rect.y << endl;
+//        cout << rect.width << endl;
+//        cout << rect.height << endl;
+        cv::Point pt1, pt2;
+        pt1.x = rect.x;
+        pt1.y = rect.y;
+        pt2.x = rect.x + rect.width;
+        pt2.y = rect.y + rect.height;
+        
+        if(rect.height>=(1.5)*rect.width){
+            if(rect.width>15 and rect.height>= 15){
+                cv::Mat player_img = cv::Mat(cameraFeed, rect);
+                cv::Mat player_hsv;
+                cv::cvtColor(player_img, player_hsv, cv::COLOR_BGR2HSV);
+                
+                //If player has blue jersy
+                cv::Mat mask1, res1;
+                cv::inRange(player_hsv, lower_blue, upper_blue, mask1);
+                cv::bitwise_and(player_img, player_img, res1, mask1);
+                cv::cvtColor(res1, res1, cv::COLOR_BGR2GRAY);
+                int nzCountBlue = cv::countNonZero(res1);
+                cout << "nzCountBlue: " << nzCountBlue << endl;
+                
+                //If player has red jersy
+                cv::Mat mask2, res2;
+                cv::inRange(player_hsv, lower_red, upper_red, mask2);
+                cv::bitwise_and(player_img, player_img, res2, mask2);
+                cv::cvtColor(res2, res2, cv::COLOR_BGR2GRAY);
+                int nzCountRed = cv::countNonZero(res2);
+                cout << "nzCountRed: " << nzCountRed << endl;
+                
+                if(nzCountBlue >= 10){
+                    cv::putText(cameraFeed, "Blue Team", pt1, font, 0.8, cv::Scalar(255,0,0), 2, cv::LINE_AA);
+                    cv::rectangle(cameraFeed, pt1, pt2, CV_RGB(0,0,255), 1);
+                }
+                if(nzCountRed >= 10){
+                    cv::putText(cameraFeed, "Red Team", pt1, font, 0.8, cv::Scalar(0,0,255), 2, cv::LINE_AA);
+                    cv::rectangle(cameraFeed, pt1, pt2, CV_RGB(255,0,0), 1);
+                }
+            }
+        }
+            
+                
+        // Draws the rect in the original image and show it
+//        cv::rectangle(cameraFeed, pt1, pt2, CV_RGB(255,0,0), 1);
+    }
+    
     return cameraFeed;
 }
 
