4653e552443ba408e2b2ab0db885e40dfd5ee9df
adrebin <alina@drebin.com>
2019-10-31 19:27:31 -0400

    adding gameinfo empty setup

---
+++ Sportsium_Skeleton/Sportsium_Skeleton/Sportsium_Skeleton/GameInfoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameInfoViewController.swift
+//  Sportsium_Skeleton
+//
+//  Created by Alina Drebin on 10/31/19.
+//  Copyright © 2019 Alina Drebin. All rights reserved.
+//
+
+import UIKit
+
+class GameInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
