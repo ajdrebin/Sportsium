41d540995d89bc1dde271c7e1c0e2f584079397b
jesscheng <jesca@umich.edu>
2019-11-12 14:16:17 -0500

    makes new  to select league and calls game info from new home page

--- Sportsium_Skeleton/Sportsium_Skeleton/Sportsium_Skeleton/ViewController.swift
+++ Sportsium_Skeleton/Sportsium_Skeleton/Sportsium_Skeleton/ViewController.swift
@@ -9,14 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    @IBOutlet var camera: UIImageView!
-    @IBOutlet var listTeams: UIImageView!
-    
 }
 
