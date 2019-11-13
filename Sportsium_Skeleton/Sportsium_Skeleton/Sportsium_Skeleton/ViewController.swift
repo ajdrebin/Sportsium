b8b8b1539d9a8859fea9ad05b2e68c024cd4f58b
jesscheng <jesca@umich.edu>
2019-11-12 19:26:25 -0500

    adds home story board, can't get linking to work

--- Sportsium_Skeleton/Sportsium_Skeleton/Sportsium_Skeleton/ViewController.swift
+++ Sportsium_Skeleton/Sportsium_Skeleton/Sportsium_Skeleton/ViewController.swift
@@ -13,5 +13,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var chooseSoccerLeague: UIImageView!
 }
 
