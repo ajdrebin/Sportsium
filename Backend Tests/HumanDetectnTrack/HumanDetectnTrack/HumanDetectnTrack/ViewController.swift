ddf19991dd9cd60b6a14ec2bf71de6b45a258f15
adrebin <alina@drebin.com>
2019-11-14 14:17:04 -0500

    apple sdk tracker

---
+++ Backend Tests/HumanDetectnTrack/HumanDetectnTrack/HumanDetectnTrack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HumanDetectnTrack
+//
+//  Created by Alina Drebin on 11/14/19.
+//  Copyright © 2019 Alina Drebin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
