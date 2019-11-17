3f37c23ae3f12d88a07325d6328950ebeb834ce2
jesscheng <jesca@umich.edu>
2019-11-17 12:21:14 -0800

    adds perform segue to get chosen league

--- Sportsium_Skeleton/Sportsium_Skeleton/Sportsium_Skeleton/ViewController.swift
+++ Sportsium_Skeleton/Sportsium_Skeleton/Sportsium_Skeleton/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var league = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let selectLeague = UITapGestureRecognizer(target: self, action: #selector(self.selectNWSL(sender:)))
+
     }
     @IBOutlet weak var chooseSoccerLeague: UIImageView!
-}
+    
+    @objc func selectNWSL(sender: UITapGestureRecognizer) {
+       print("Please Help!")
+    }
+    
+    
+//
+//    @IBAction func selectNWSL(sender: UITapGestureRecognizer) {
+//
+//        league = "NWSL"
+//        print("league")
+//        print(league)
+//        performSegue(withIdentifier: "chooseLeague", sender: self)
+//    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "chooseLeague"){
+                let displayVC = segue.destination as! HomeViewController
+                displayVC.chosenLeague = league
+        }
+    }
 
+}
