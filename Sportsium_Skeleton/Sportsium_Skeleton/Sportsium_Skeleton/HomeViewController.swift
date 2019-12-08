2d97f99331e5dcb3a01c49bff63e3c5b800cfdb6
Gustavo Ramirez <grami@umich.edu>
2019-12-08 12:56:31 -0500

    Moved API to the front

--- Sportsium_Skeleton/Sportsium_Skeleton/Sportsium_Skeleton/HomeViewController.swift
+++ Sportsium_Skeleton/Sportsium_Skeleton/Sportsium_Skeleton/HomeViewController.swift
@@ -8,128 +8,7 @@
 
 import UIKit
 
-struct WNBA: Codable {
-     let atlantaDream: TeamInfo
-     let chicagoSky: TeamInfo
-     let connecticutSun: TeamInfo
-     let indianaFever: TeamInfo
-     let newYorkLiberty: TeamInfo
-     let washingtonMystics: TeamInfo
-     let dallasWings: TeamInfo
-     let lasVegasAces: TeamInfo
-     let losAngelesSparks: TeamInfo
-     let minnesotaLynx: TeamInfo
-     let phoenixMercury: TeamInfo
-     let seattleStorm: TeamInfo
-
-     enum CodingKeys: String, CodingKey {
-        case atlantaDream = "atlanta_dream"
-        case chicagoSky = "chicago_sky"
-        case connecticutSun = "connecticut_sun"
-        case indianaFever = "indiana_fever"
-        case newYorkLiberty = "new_york_liberty"
-        case washingtonMystics = "washington_mystics"
-        case dallasWings = "dallas_wings"
-        case lasVegasAces = "las_vegas_aces"
-        case losAngelesSparks = "los_angeles_sparks"
-        case minnesotaLynx = "minnesota_lynx"
-        case phoenixMercury = "phoenix_mercury"
-        case seattleStorm = "seattle_storm"
-    }
-}
-struct NWSL: Codable {
-     let orlandoPride: TeamInfo
-     let skyBlue: TeamInfo
-     let houstonDash: TeamInfo
-     let washingtonSpirit: TeamInfo
-     let northCarolinaCourage: TeamInfo
-     let reign: TeamInfo
-     let portlandThorns: TeamInfo
-     let chicagoRedStars: TeamInfo
-     let utahRoyals: TeamInfo
-     
-     enum CodingKeys: String, CodingKey {
-        case orlandoPride = "orlando_pride"
-        case skyBlue = "sky_blue"
-        case houstonDash = "houston_dash"
-        case washingtonSpirit = "washington_spirit"
-        case northCarolinaCourage = "north_carolina_courage"
-        case reign = "reign"
-        case portlandThorns = "portland_thorns"
-        case chicagoRedStars = "chicago_red_stars"
-        case utahRoyals = "utah_royals"
-    }
-}
-
- struct TeamInfo: Codable {
-     let cityLocation: String
-     let league: String
-     let dateFounded: String
-     let instagram: String
-     let currentWins: String
-     let twitter: String
-     let snapchat: String
-     let currentTies: String
-     let currentLosses: String
-     let fb: String
-     let headCoach: String
-     let stadium: String
-     let teamName: String
-     let playerList: [Player]
-
-     enum CodingKeys: String, CodingKey {
-         case cityLocation = "city_location"
-         case league
-         case dateFounded = "date_founded"
-         case instagram
-         case currentWins = "current_wins"
-         case twitter, snapchat
-         case currentTies = "current_ties"
-         case currentLosses = "current_losses"
-         case fb
-         case headCoach = "head_coach"
-         case stadium
-         case teamName = "team_name"
-         case playerList = "player_list"
-     }
- }
-
- struct Player: Codable {
-     let firstName: String
-     let lastName: String
-     let instagram: String
-     let hometown: String
-     let twitter: String
-     let number: String
-     let snapchat: String
-     let height: String
-     let fb: String
-     let DOB: String
-     let country: String
-     let playerId: Int
-     let position: String
-
-     enum CodingKeys: String, CodingKey {
-         case firstName = "first_name"
-         case lastName = "last_name"
-         case instagram, hometown, twitter, number, snapchat, height, fb
-         case DOB = "date_of_birth"
-         case country
-         case playerId = "player_id"
-         case position
-     }
- }
-
-
 // GLOBAL VARIABLES
-var teamsDict = [String: TeamInfo]()
-
-var initiate = TeamInfo(cityLocation: "", league: "", dateFounded: "", instagram: "", currentWins: "", twitter: "", snapchat: "", currentTies: "", currentLosses: "", fb: "", headCoach: "", stadium: "", teamName: "", playerList: [])
-
-var NWSLteams = NWSL(orlandoPride: initiate, skyBlue: initiate, houstonDash: initiate, washingtonSpirit: initiate, northCarolinaCourage: initiate, reign: initiate, portlandThorns: initiate, chicagoRedStars: initiate, utahRoyals: initiate)
-var WNBAteams = WNBA(atlantaDream: initiate, chicagoSky: initiate, connecticutSun: initiate, indianaFever: initiate, newYorkLiberty: initiate, washingtonMystics: initiate, dallasWings: initiate, lasVegasAces: initiate, losAngelesSparks: initiate, minnesotaLynx: initiate, phoenixMercury: initiate, seattleStorm: initiate)
-    
-var league = ""
 
 var home = ""
 var away = ""
@@ -147,7 +26,6 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var game1: UIButton!
     @IBOutlet weak var game2: UIButton!
     @IBOutlet weak var game3: UIButton!
-    var didLoad = false
     
     @IBOutlet weak var game1home: UIImageView!
     @IBOutlet weak var game1away: UIImageView!
@@ -208,52 +86,6 @@ class HomeViewController: UIViewController {
            }
        }
 
-        let urlString = "http://159.89.139.18/sports_check/?league=" + league
-        guard let url = URL(string: urlString) else { return }
-
-        // 2
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if error != nil {
-                print(error!.localizedDescription)
-            }
-
-            guard let data = data else { return }
-            do {
-                // 3
-                //Decode data
-              print(league)
-                if (league == "NWSL") {
-                    NWSLteams = try JSONDecoder().decode(NWSL.self, from: data)
-
-                    teamsDict["orlando_pride"] = NWSLteams.orlandoPride
-                    teamsDict["sky_blue"] = NWSLteams.skyBlue
-                    teamsDict["houston_dash"] = NWSLteams.houstonDash
-                    teamsDict["washington_spirit"] = NWSLteams.washingtonSpirit
-                    teamsDict["north_carolina_courage"] = NWSLteams.northCarolinaCourage
-                    teamsDict["reign"] = NWSLteams.reign
-                    teamsDict["portland_thorns"] = NWSLteams.portlandThorns
-                    teamsDict["chicago_red_stars"] = NWSLteams.chicagoRedStars
-                    teamsDict["utah_royals"] = NWSLteams.utahRoyals
-                }
-                else {
-                    WNBAteams = try JSONDecoder().decode(WNBA.self, from: data)
-
-                    teamsDict["atlanta_dream"] = WNBAteams.atlantaDream
-                    teamsDict["chicago_sky"] = WNBAteams.chicagoSky
-                    teamsDict["connecticut_sun"] = WNBAteams.connecticutSun
-                    teamsDict["indiana_fever"] = WNBAteams.indianaFever
-                    teamsDict["new_york_liberty"] = WNBAteams.newYorkLiberty
-                    teamsDict["washington_mystics"] = WNBAteams.washingtonMystics
-                    teamsDict["dallas_wings"] = WNBAteams.dallasWings
-                    teamsDict["las_vegas_aces"] = WNBAteams.lasVegasAces
-                    teamsDict["los_angeles_sparks"] = WNBAteams.losAngelesSparks
-                    teamsDict["minnesota_lynx"] = WNBAteams.minnesotaLynx
-                    teamsDict["phoenix_mercury"] = WNBAteams.phoenixMercury
-                    teamsDict["seattle_storm"] = WNBAteams.seattleStorm
-                }
-                
-
-                
                 if(league == "NWSL"){
                     // Set default home and away
                     home = "orlando_pride"
@@ -265,18 +97,6 @@ class HomeViewController: UIViewController {
                     away = "chicago_sky"
                 }
 
-                
-                
-                // 4
-                
-
-            } catch let jsonError {
-                print(jsonError)
-            }
-            // 5
-            self.didLoad = true
-            }.resume()
-        
          }
      @objc func buttonAction(sender: UIButton!) {
        let btn: UIButton = sender
@@ -332,9 +152,6 @@ class HomeViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if(segue.identifier == "Game"){
-            if(!self.didLoad){
-                return
-            }
             let displayVC = segue.destination as! GameInfoViewController
             displayVC.home = home
             displayVC.away = away
--- Sportsium_Skeleton/Sportsium_Skeleton/Sportsium_Skeleton/ViewController.swift
+++ Sportsium_Skeleton/Sportsium_Skeleton/Sportsium_Skeleton/ViewController.swift
@@ -8,43 +8,234 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    var league = ""
-  
-  @IBOutlet weak var wnba_button: UIButton!
-  @IBOutlet weak var nwsl_button: UIButton!
+var league = ""
 
-  
+var teamsDict = [String: TeamInfo]()
+
+var initiate = TeamInfo(cityLocation: "", league: "", dateFounded: "", instagram: "", currentWins: "", twitter: "", snapchat: "", currentTies: "", currentLosses: "", fb: "", headCoach: "", stadium: "", teamName: "", playerList: [])
+
+var NWSLteams = NWSL(orlandoPride: initiate, skyBlue: initiate, houstonDash: initiate, washingtonSpirit: initiate, northCarolinaCourage: initiate, reign: initiate, portlandThorns: initiate, chicagoRedStars: initiate, utahRoyals: initiate)
+var WNBAteams = WNBA(atlantaDream: initiate, chicagoSky: initiate, connecticutSun: initiate, indianaFever: initiate, newYorkLiberty: initiate, washingtonMystics: initiate, dallasWings: initiate, lasVegasAces: initiate, losAngelesSparks: initiate, minnesotaLynx: initiate, phoenixMercury: initiate, seattleStorm: initiate)
+
+struct WNBA: Codable {
+    let atlantaDream: TeamInfo
+    let chicagoSky: TeamInfo
+    let connecticutSun: TeamInfo
+    let indianaFever: TeamInfo
+    let newYorkLiberty: TeamInfo
+    let washingtonMystics: TeamInfo
+    let dallasWings: TeamInfo
+    let lasVegasAces: TeamInfo
+    let losAngelesSparks: TeamInfo
+    let minnesotaLynx: TeamInfo
+    let phoenixMercury: TeamInfo
+    let seattleStorm: TeamInfo
+    
+    enum CodingKeys: String, CodingKey {
+        case atlantaDream = "atlanta_dream"
+        case chicagoSky = "chicago_sky"
+        case connecticutSun = "connecticut_sun"
+        case indianaFever = "indiana_fever"
+        case newYorkLiberty = "new_york_liberty"
+        case washingtonMystics = "washington_mystics"
+        case dallasWings = "dallas_wings"
+        case lasVegasAces = "las_vegas_aces"
+        case losAngelesSparks = "los_angeles_sparks"
+        case minnesotaLynx = "minnesota_lynx"
+        case phoenixMercury = "phoenix_mercury"
+        case seattleStorm = "seattle_storm"
+    }
+}
+struct NWSL: Codable {
+    let orlandoPride: TeamInfo
+    let skyBlue: TeamInfo
+    let houstonDash: TeamInfo
+    let washingtonSpirit: TeamInfo
+    let northCarolinaCourage: TeamInfo
+    let reign: TeamInfo
+    let portlandThorns: TeamInfo
+    let chicagoRedStars: TeamInfo
+    let utahRoyals: TeamInfo
+    
+    enum CodingKeys: String, CodingKey {
+        case orlandoPride = "orlando_pride"
+        case skyBlue = "sky_blue"
+        case houstonDash = "houston_dash"
+        case washingtonSpirit = "washington_spirit"
+        case northCarolinaCourage = "north_carolina_courage"
+        case reign = "reign"
+        case portlandThorns = "portland_thorns"
+        case chicagoRedStars = "chicago_red_stars"
+        case utahRoyals = "utah_royals"
+    }
+}
+
+struct TeamInfo: Codable {
+    let cityLocation: String
+    let league: String
+    let dateFounded: String
+    let instagram: String
+    let currentWins: String
+    let twitter: String
+    let snapchat: String
+    let currentTies: String
+    let currentLosses: String
+    let fb: String
+    let headCoach: String
+    let stadium: String
+    let teamName: String
+    let playerList: [Player]
+    
+    enum CodingKeys: String, CodingKey {
+        case cityLocation = "city_location"
+        case league
+        case dateFounded = "date_founded"
+        case instagram
+        case currentWins = "current_wins"
+        case twitter, snapchat
+        case currentTies = "current_ties"
+        case currentLosses = "current_losses"
+        case fb
+        case headCoach = "head_coach"
+        case stadium
+        case teamName = "team_name"
+        case playerList = "player_list"
+    }
+}
+
+struct Player: Codable {
+    let firstName: String
+    let lastName: String
+    let instagram: String
+    let hometown: String
+    let twitter: String
+    let number: String
+    let snapchat: String
+    let height: String
+    let fb: String
+    let DOB: String
+    let country: String
+    let playerId: Int
+    let position: String
+    
+    enum CodingKeys: String, CodingKey {
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case instagram, hometown, twitter, number, snapchat, height, fb
+        case DOB = "date_of_birth"
+        case country
+        case playerId = "player_id"
+        case position
+    }
+}
+
+class ViewController: UIViewController {
+    
+    var didLoad = false
+    
+    @IBOutlet weak var wnba_button: UIButton!
+    @IBOutlet weak var nwsl_button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-      
+        
         nwsl_button.tag = 1
         wnba_button.tag = 2
         nwsl_button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
         wnba_button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
     }
-  
+    
     @objc func buttonAction(sender: UIButton!) {
-      let btn: UIButton = sender
-   
-      if btn.tag == 1 {
-        league = "NWSL"
-      }
-      else{
-        league = "WNBA"
-      }
-      performSegue(withIdentifier: "Home", sender: self)
+        let btn: UIButton = sender
+        teamsDict.removeAll()
+        
+        if btn.tag == 1 {
+            league = "NWSL"
+            let urlString = "http://159.89.139.18/sports_check/?league=" + league
+            guard let url = URL(string: urlString) else { return }
+            
+            // 2
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!.localizedDescription)
+                }
+                
+                guard let data = data else { return }
+                do {
+                    // 3
+                    //Decode data
+                    NWSLteams = try JSONDecoder().decode(NWSL.self, from: data)
+                    
+                    teamsDict["orlando_pride"] = NWSLteams.orlandoPride
+                    teamsDict["sky_blue"] = NWSLteams.skyBlue
+                    teamsDict["houston_dash"] = NWSLteams.houstonDash
+                    teamsDict["washington_spirit"] = NWSLteams.washingtonSpirit
+                    teamsDict["north_carolina_courage"] = NWSLteams.northCarolinaCourage
+                    teamsDict["reign"] = NWSLteams.reign
+                    teamsDict["portland_thorns"] = NWSLteams.portlandThorns
+                    teamsDict["chicago_red_stars"] = NWSLteams.chicagoRedStars
+                    teamsDict["utah_royals"] = NWSLteams.utahRoyals
+                    
+                } catch let jsonError {
+                    print(jsonError)
+                }
+                // 5
+                self.didLoad = true
+            }.resume()
+        }
+        else{
+            league = "WNBA"
+            let urlString = "http://159.89.139.18/sports_check/?league=" + league
+            guard let url = URL(string: urlString) else { return }
+            
+            // 2
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!.localizedDescription)
+                }
+                
+                guard let data = data else { return }
+                do {
+                    // 3
+                    //Decode data
+                    WNBAteams = try JSONDecoder().decode(WNBA.self, from: data)
+                    
+                    teamsDict["atlanta_dream"] = WNBAteams.atlantaDream
+                    teamsDict["chicago_sky"] = WNBAteams.chicagoSky
+                    teamsDict["connecticut_sun"] = WNBAteams.connecticutSun
+                    teamsDict["indiana_fever"] = WNBAteams.indianaFever
+                    teamsDict["new_york_liberty"] = WNBAteams.newYorkLiberty
+                    teamsDict["washington_mystics"] = WNBAteams.washingtonMystics
+                    teamsDict["dallas_wings"] = WNBAteams.dallasWings
+                    teamsDict["las_vegas_aces"] = WNBAteams.lasVegasAces
+                    teamsDict["los_angeles_sparks"] = WNBAteams.losAngelesSparks
+                    teamsDict["minnesota_lynx"] = WNBAteams.minnesotaLynx
+                    teamsDict["phoenix_mercury"] = WNBAteams.phoenixMercury
+                    teamsDict["seattle_storm"] = WNBAteams.seattleStorm
+                    
+                } catch let jsonError {
+                    print(jsonError)
+                }
+                // 5
+                self.didLoad = true
+            }.resume()
+        }
+        performSegue(withIdentifier: "Home", sender: self)
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if(segue.identifier == "Home"){
-                let displayVC = segue.destination as! HomeViewController
+            if(!self.didLoad){
+                return
+            }
+            let displayVC = segue.destination as! HomeViewController
             print("league: ", league)
-                displayVC.chosenLeague = league
-              print("In viewcontroller")
-              print(league)
+            displayVC.chosenLeague = league
+            print("In viewcontroller")
+            print(league)
         }
     }
-
+    
 }
